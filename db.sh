71626621a0f8b8d106d93bca97038264fa3c59b3
homma <k014c1108@it-neec.jp>
2016-06-06 22:48:12 +0900

    first

--- db.sh
+++ db.sh
@@ -1,1 +1,3 @@
-特になし
+mysql -uroot -e "drop database todolist;"
+mysql -uroot -e "create database todolist character set UTF8;"
+mysql -uroot todolist -e "create table list(id MEDIUMINT NOT NULL AUTO_INCREMENT, item VARCHAR(256), PRIMARY KEY(id));"
